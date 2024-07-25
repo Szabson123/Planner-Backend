cc2f54dc86e7f86f65a2ce426bbe13b5f2c454bb
Szabson123 <szymonzaba18@gmail.com>
2024-07-25 10:11:57 +0200

    generating events to next friday

--- plan/serializers.py
+++ plan/serializers.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
-from .models import Event, Shift
+from .models import Event, Shift, WorkingHours
 from custom_user.serializers import UserSerializer
+from custom_user.models import CustomUser
 
+
+class WorkingHoursSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = WorkingHours
+        fields = ['id', 'start_time', 'end_time']
+        
 
 class ShiftSerializer(serializers.ModelSerializer):
-    users = UserSerializer(many=True, read_only=True)
+    users = serializers.PrimaryKeyRelatedField(queryset=CustomUser.objects.all(), many=True)
 
     class Meta:
         model = Shift
-        fields = ['id', 'name', 'description', 'users', 'start_date', 'end_date']
+        fields = ['id', 'name', 'description', 'users']
 
 
 class EventSerializer(serializers.ModelSerializer):
--- plan/views.py
+++ plan/views.py
@@ -20,24 +20,32 @@ class ShiftViewSet(viewsets.ModelViewSet):
     queryset = Shift.objects.all()
     
 
-class GeneratePlannerView(GenericAPIView):
+class GeneratePlannerView(APIView):
     serializer_class = EventSerializer
     
     def post(self, request, *args, **kwargs):
         shifts = Shift.objects.all()
         generated_events = []
         today = datetime.now().date()
-        next_day = today + timedelta(days=1)
 
-        for shift in shifts:
-            users = shift.users.all()
-            for user in users:
-                event = Event.objects.create(
-                    user=user,
-                    date=next_day,
-                    shift=shift
-                )
-                generated_events.append(event)
+        days_until_friday = (4 - today.weekday()) % 7  
+        if days_until_friday == 0:
+            days_until_friday = 7
+        
+        end_date = today + timedelta(days=days_until_friday)
+
+        current_date = today
+        while current_date <= end_date:
+            for shift in shifts:
+                users = shift.users.all()
+                for user in users:
+                    event = Event.objects.create(
+                        user=user,
+                        date=current_date,
+                        shift=shift
+                    )
+                    generated_events.append(event)
+            current_date += timedelta(days=1)
                 
         serializer = EventSerializer(generated_events, many=True)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
